4e42f60a170c9c84e8cadb03da16eb0f90228c0f
NuttawatJuntawong <91516482+NuttawatJuntawong@users.noreply.github.com>
2022-10-09 13:45:09 +0700

    Update minimax.js

--- test/minimax.js
+++ test/minimax.js
@@ -68,3 +68,40 @@ function bestMove() {
       return bestScore;
     }
   }
+
+  function noobminimax(board, depth, isMaximizing) { //botกาก
+    let result = checkWinner();
+    if (result !== null) {
+      return scores[result];
+    }
+  
+    if (isMaximizing) {
+      let bestScore = -Infinity;
+      for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+          // Is the spot available?
+          if (board[i][j] == '') {
+            board[i][j] = ai;
+            let score = minimax(board, depth + 1, false);
+            board[i][j] = '';
+            bestScore = min(score, bestScore);
+          }
+        }
+      }
+      return bestScore;
+    } else {
+      let bestScore = Infinity;
+      for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+          // Is the spot available?
+          if (board[i][j] == '') {
+            board[i][j] = human;
+            let score = minimax(board, depth + 1, true);
+            board[i][j] = '';
+            bestScore = max(score, bestScore);
+          }
+        }
+      }
+      return bestScore;
+    }
+  }
