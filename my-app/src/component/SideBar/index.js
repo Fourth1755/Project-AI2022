0dd4e73db4485d335700525fa90783cf8dc1dee6
Fourth1755 <fourthe.hero@gmail.com>
2022-10-10 23:07:40 +0700

    Update index.js

--- my-app/src/component/SideBar/index.js
+++ my-app/src/component/SideBar/index.js
@@ -5,9 +5,7 @@ const SideBar = (props) => {
 
   return (
     <div className="sidebar-container">
-      <h1>Menu</h1>
       <div class="mb-3">
-        <label for="exampleFormControlInput1" class="form-label">Level</label>
         <select class="form-select" aria-label="Default select example">
             <option selected>Open this select Level</option>
             <option value="1">EASY</option>
